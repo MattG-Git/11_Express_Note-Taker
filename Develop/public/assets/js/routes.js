3071d7e4e70382119a4904337673d4b531ba8cf2
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-09-05 08:37:29 -0700

    added comments

--- Develop/public/assets/js/routes.js
+++ Develop/public/assets/js/routes.js
@@ -1,7 +1,8 @@
+// Required dependencies
 const fs = require('fs');
-
 const router = require('express').Router();
 
+// route to get all of the notes within the db.json database
 router.get('/notes', (req, res) => {
     console.log('Start adding your notes');
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -13,16 +14,19 @@ router.get('/notes', (req, res) => {
     })
 });
 
+// route to add new notes within the db.json database
 router.post('/notes', (req, res) => {
     console.log('Saving your notes');
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         let allNotes = JSON.parse(data);
 
+// save a new note request to an object that is pushed to the notes array within the db.json file with a randomized id
         const {title, text} = req.body;
         if (req.body){
             const newNote = {
                 title,
                 text,
+                //assigns a random number as a string to the id property of the new note. This is used later in the delete route
                 id: Math.floor((1 + Math.random()) * 100).toString()
             };
             console.log(newNote.id);
@@ -39,6 +43,7 @@ router.post('/notes', (req, res) => {
     })
 });
 
+//this retreives a note by the id then filters it out from the db.json file so that it is no longer part of the database
 router.delete('/notes/:id', (req, res) => {
     // console.log(req);
     if(req) {
